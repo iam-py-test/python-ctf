9c95526811657ede03c0e0ff45086c36da84f359
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-12-28 09:24:47 -0500

    Create magic-number.py

---
+++ magic-number/magic-number.py
@@ -0,0 +1,23 @@
+import random
+import os
+playing = True
+points = 0
+while playing:
+  randnumb = random.choice([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55])
+  gotit = False
+  while gotit == False:
+    cmd = input("What's the magic number? ")
+    if cmd == str(randnumb):
+      print("Got it! ")
+      gotit = True
+      points += 1
+    elif cmd.startswith("math: ")
+      ma = cmd.split("math: ")[0]
+      print("{}".format(eval(ma)))
+    elif cmd == "getpoints":
+      print("Points: {}".format(points))
+    elif cmd == "end":
+      gotit = None
+      playing = False
+print("---- Game over ----")
+print("Points: {}".format(points))
