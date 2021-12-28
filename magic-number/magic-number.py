4f1bc6bd2957c17de065bec4a9626c5ac0d45175
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-12-28 10:11:31 -0500

    New commands

--- magic-number/magic-number.py
+++ magic-number/magic-number.py
@@ -2,6 +2,16 @@ import random
 import os
 playing = True
 points = 0
+
+cmdhelp = """
+[number]: Check if that number is the answer
+math: Evaluate an math expression (i.e. 1+1)
+getpoints: Display the current points
+reset: Reset the game
+end: End the game
+help: Display this help message
+"""
+
 while playing:
   randnumb = random.choice([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55])
   gotit = False
@@ -11,8 +21,8 @@ while playing:
       print("Got it! ")
       gotit = True
       points += 1
-    elif cmd.startswith("math: "):
-      ma = cmd.replace("math: ","")
+    elif cmd.startswith("math "):
+      ma = cmd.replace("math ","")
       try:
         evaled = eval(ma)
         print("{}".format(evaled))
@@ -21,9 +31,22 @@ while playing:
         continue
     elif cmd == "getpoints":
       print("Points: {}".format(points))
+    elif cmd == 'reset':
+      gotit = True
+      points = 0
     elif cmd == "end":
       gotit = None
       playing = False
+    elif cmd == "help":
+      print(cmdhelp)
+    else:
+      try:
+        exec("int({})".format(cmd))
+      except:
+        print("Command {} not found".format(cmd))
+        print(cmdhelp)
+      else:
+        print("Wrong number")
 
 print("---- Game over ----")
 print("Points: {}".format(points))
